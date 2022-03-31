5f2a32a1323f0f0e7334a1106552712b4f933cc9
Andrey <andrew25555@yandex.ru>
2022-03-31 14:49:00 +0300

    Добавьте файлы проекта.

---
+++ ReadMe.md
@@ -0,0 +1,20 @@
+<h1>Отчёт за 25 марта</h1>
+<h2>Выполнили:<br><b>Головин А.Д.<br>Юлин А.А.</b></h2>
+<h3>Окно авторизации</h3>
+<img src="https://user-images.githubusercontent.com/98512815/160254398-4ce71a09-32f4-41aa-bd7a-d24a77740f9d.gif">
+<br>
+<h4>Дизайн мы разработали на окне Products, но мы не успевали, поэтому не поставили его на другие окна</h4>
+<br>
+<h3>Окно Employee</h3>
+<img src="https://user-images.githubusercontent.com/98512815/160254482-0831fd8e-fe91-4467-8340-0349a2e33952.gif">
+<br>
+<h3>Окно Clients</h3>
+<img src="https://user-images.githubusercontent.com/98512815/160254486-ec0ea357-6107-413a-b8ba-defa49e16d3c.gif">
+<br>
+<h3>Окно Products</h3>
+<h4>Дизайн мы разработали на окне Products, но мы не успевали, поэтому не поставили его на другие окна</h4>
+<img src="https://user-images.githubusercontent.com/98512815/160254497-dbafe663-0a74-4970-8d46-addfc559c2e5.gif">
+<br>
+<h3>Окно Sales</h3>
+<img src="https://user-images.githubusercontent.com/98512815/160254499-3f56c8a0-8fbe-4c52-9c4d-97395c57cc20.gif">
+<br>
---
+++ Windows/AddEquipmentWindow.xaml.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using EquipmentRent3ISP9_7.EF;
+using Microsoft.Win32;
+
+namespace EquipmentRent3ISP9_7.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEquipmentWindow.xaml
+    /// </summary>
+    public partial class AddEquipmentWindow : Window
+    {
+        private bool isEdit;
+        Product editProduct = new Product();
+        private string pathPhoto = null;
+
+        public AddEquipmentWindow()
+        {
+            InitializeComponent();
+            cmbProductName.ItemsSource = HelperClass.HelperCl.Context.Product.ToList();
+            cmbProductName.DisplayMemberPath = "ProductName";
+
+            cmbManufacturerName.ItemsSource = HelperClass.HelperCl.Context.Manufacturer.ToList();
+            cmbManufacturerName.DisplayMemberPath = "ManufacturerName";
+
+            isEdit = false;
+        }
+
+        public AddEquipmentWindow(Product product)
+        {
+            InitializeComponent();
+
+            // Заполнение полей свойствами аргумента employee 
+            cmbProductName.ItemsSource = HelperClass.HelperCl.Context.Product.ToList();
+            cmbProductName.DisplayMemberPath = "ProductName";
+
+            cmbManufacturerName.ItemsSource = HelperClass.HelperCl.Context.Manufacturer.ToList();
+            cmbManufacturerName.DisplayMemberPath = "ManufacturerName";
+
+            cmbProductName.SelectedIndex = product.IdProduct - 1;
+            cmbManufacturerName.SelectedIndex = product.IdManufacturer - 1;
+
+            txtPrice.Text = product.Price.ToString();
+            dpWarranty.SelectedDate = product.Warranty;
+
+            if (product.Photo != null)
+            {
+                using (MemoryStream stream = new MemoryStream(product.Photo))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    bitmapImage.BeginInit();
+                    bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                    bitmapImage.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                    bitmapImage.StreamSource = stream;
+                    bitmapImage.EndInit();
+
+                    productPhoto.Source = bitmapImage;
+                }
+            }
+
+            tbTitle.Text = "Изменение данных оборудования";
+            btnAddNew.Content = "Сохранить";
+
+            isEdit = true;
+            // Сохраняем employee для доступа вне конструктора
+            editProduct = product;
+        }
+
+        private void btnAddNew_Click(object sender, RoutedEventArgs e)
+        {
+            #region Validation
+            // Null or White Space
+            if (string.IsNullOrWhiteSpace(txtPrice.Text))
+            {
+                MessageBox.Show("Поле стоимость пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(dpWarranty.SelectedDate.ToString()))
+            {
+                MessageBox.Show("Поле гарантия пустое!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            decimal savePrice = 0;
+            try
+            {
+                savePrice = Convert.ToDecimal(txtPrice.Text);
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Стоимость введена некорректно!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (savePrice <= 0)
+            {
+                MessageBox.Show("Стоимость введена некорректно!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            #endregion
+
+            if (isEdit)
+            {
+                // Обработка случайного нажатия
+                MessageBoxResult resClick = MessageBox.Show("Изменить параметры оборудования?", "Подтверждение редактирования", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resClick == MessageBoxResult.No)
+                {
+                    return;
+                }
+                try
+                {
+                    editProduct.IdManufacturer = (cmbManufacturerName.SelectedItem as Manufacturer).IdManufacturer;
+                    editProduct.Price = savePrice;
+                    editProduct.Warranty = Convert.ToDateTime(dpWarranty.SelectedDate);
+
+                    if (pathPhoto != null)
+                    {
+                        editProduct.Photo = File.ReadAllBytes(pathPhoto);
+                    }
+
+                    HelperClass.HelperCl.Context.SaveChanges();
+                    MessageBox.Show("Параметры оборудования изменены!", "Редактирование");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка!");
+                }
+            }
+            else
+            {
+                // Обработка случайного нажатия
+                MessageBoxResult resClick = MessageBox.Show("Добавить пользователя?", "Подтверждение добавления", MessageBoxButton.YesNo, MessageBoxImage.Question);
+                if (resClick == MessageBoxResult.No)
+                {
+                    return;
+                }
+
+                try
+                {
+                    Product newProduct = new Product
+                    {
+                        IdManufacturer = (cmbManufacturerName.SelectedItem as Manufacturer).IdManufacturer,
+                        Price = savePrice,
+                        Warranty = Convert.ToDateTime(dpWarranty.SelectedDate)
+                    };
+
+                    if (pathPhoto != null)
+                    {
+                        newProduct.Photo = File.ReadAllBytes(pathPhoto);
+                    }
+
+                    HelperClass.HelperCl.Context.Product.Add(newProduct);
+
+                    HelperClass.HelperCl.Context.SaveChanges();
+
+                    MessageBox.Show("Новое оборудование добавлено!", "Добавление");
+                    Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString(), "Ошибка!");
+                }
+            }
+        }
+        // Comment
+
+        private void btnChoosePhoto_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+            if (openFile.ShowDialog() == true)
+            {
+                productPhoto.Source = new BitmapImage(new Uri(openFile.FileName));
+                pathPhoto = openFile.FileName;
+            }
+        }
+    }
+}
